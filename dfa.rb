76711375316b91f32d2bc9a43870f53643273f31
Andrei Merfu <andrei.merfu@my.fmi.unibuc.ro>
2019-03-23 10:21:34 +0200

    Fix open input file just one time

--- dfa.rb
+++ dfa.rb
@@ -1,9 +1,10 @@
 require_relative "helper_methods"
 require_relative "dfa_state"
 require_relative "dfa_transitions"
+require "pry"
 
 class Dfa
-  attr_accessor :fp, :file_name, :transitions, :file_position
+  attr_accessor :fp, :file_name, :transitions, :file_position, :file_content
 
   STATE = DFA_STATE
 
@@ -11,36 +12,34 @@ class Dfa
     @file_name = file_name
     @transitions = dfa_transitions
     @file_position = 0
+    @file_content = File.read(file_name)
   end
 
   def execute
     current_state = STATE[:initial]
     token_value = ""
 
-    File.open(file_name, "r") do |f|
-      f.seek(@file_position, IO::SEEK_SET)
-      f.each_char do |c|
-        @file_position += 1
-        state_transitions = transitions[current_state]
+    file_content[file_position..-1].each_char do |c|
+      @file_position += 1
+      state_transitions = transitions[current_state]
 
-        state_transitions&.each do |transition|
-          transition_function = transition.first
-          next_state = transition.last
+      state_transitions&.each do |transition|
+        transition_function = transition.first
+        next_state = transition.last
 
-          if (transition_function.call(c))
-            if next_state == STATE[:error]
-              return [STATE[:error], token_value]
-            end
-
-            if (next_state == STATE[:end])
-              @file_position -= 1
-              return [current_state, token_value]
-            end
-
-            current_state = next_state
-            token_value += c
-            break
+        if (transition_function.call(c))
+          if next_state == STATE[:error]
+            return [STATE[:error], token_value]
           end
+
+          if (next_state == STATE[:end])
+            @file_position -= 1
+            return [current_state, token_value]
+          end
+
+          current_state = next_state
+          token_value += c
+          break
         end
       end
     end
